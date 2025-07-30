9bfb91186abd13a544642cc695b27fa630c093b0
rsonofre <rafaelonofre@gmail.com>
2025-07-30 20:20:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function gerarBusca() {
     const comparacao = document.getElementById("comparacao").value;
     const desfecho = document.getElementById("desfecho").value;
 
-    const resposta = await fetch("https://gerador-busca-pubmed-backend.onrender.com/gerar_busca", {
+    const resposta = await fetch("https://gerador-novo.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ populacao, intervencao, comparacao, desfecho })
