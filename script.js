d1c58773a8c34c3cccdc3572bc21323ba7cb7945
rsonofre <rafaelonofre@gmail.com>
2025-08-01 16:52:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
 async function gerarBusca() {
     const populacao = document.getElementById("populacao").value;
     const intervencao = document.getElementById("intervencao").value;
     const comparacao = document.getElementById("comparacao").value;
     const desfecho = document.getElementById("desfecho").value;
 
-    const resposta = await fetch("https://gerador-novo.onrender.com", {
+    const resposta = await fetch("https://gerador-novo.onrender.com/gerar", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ populacao, intervencao, comparacao, desfecho })
     });
 
     const data = await resposta.json();
-    document.getElementById("resultado").textContent = data.resposta || data.erro;
+    document.getElementById("resultado").textContent = data.resultado || data.erro;
 }
