9012eed5090666ac824b66bf4e9cf10158c32d9b
rsonofre <rafaelonofre@gmail.com>
2025-08-01 17:08:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,8 @@ Informações fornecidas:
         return jsonify({"erro": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
